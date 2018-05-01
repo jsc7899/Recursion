754d65a7101f977dffb2e1c3447faa4fea046dac
jsc7899 <jsc7899@users.noreply.github.com>
2018-05-01 15:30:10 -0500

    add files

---
+++ cmake-build-debug/CMakeFiles/Proj6test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jared/Documents/Testing/Proj6test/Project6.cpp" "/home/jared/Documents/Testing/Proj6test/cmake-build-debug/CMakeFiles/Proj6test.dir/Project6.cpp.o"
+  "/home/jared/Documents/Testing/Proj6test/main.cpp" "/home/jared/Documents/Testing/Proj6test/cmake-build-debug/CMakeFiles/Proj6test.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Proj6test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proj6test.dir/main.cpp.o"
+  "CMakeFiles/Proj6test.dir/Project6.cpp.o"
+  "Proj6test.pdb"
+  "Proj6test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proj6test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
